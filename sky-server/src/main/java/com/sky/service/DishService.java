325244852dfd62bfde65f1b2132b9f472fda43b8
IvyCode <kbdw2021@leeds.ac.uk>
2026-07-19 12:53:28 +0800

    feat: add dish pagination query

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,10 +1,8 @@
 package com.sky.service;
-
 import com.sky.dto.DishDTO;
-import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
+
 
 public interface DishService {
 
@@ -12,7 +10,12 @@ public interface DishService {
      * 新增菜品，和对应的口味数据
      * @param dishDTO
      */
-
     void saveWithFlavor(DishDTO dishDTO);
 
+    /**
+     * 菜品分页查询
+     * @param dishPageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
